e3dff5ffe50f1abee8b63801fe6ba0ad4390e81e
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-12-08 09:02:59 +0300

    Completed assignment

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,28 @@
+// action = {
+//   type: "friends/add",
+//   payload: {
+//     name: "Chrome Boi"
+//     homewtown: "NYC",
+//     id: 1
+//   }
+// }
+
+// action = {
+//   type: "friends/remove",
+//   payload: 1,
+// };
+
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type){
+    case "friends/add":
+      return{
+        friends: [...state.friends, action.payload],
+      };
+      case "friends/remove":
+        return {
+          friends: state.friends.filter((friend) => friend.id !== action.payload),
+        };
+        default:
+          return state;
+  }
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,15 @@
+// action = {
+//   type: "presents/increase",
+// }
+
 const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
-  // your code here
+  switch (action.type) {
+    case "presents/increase":
+    return{ numberOfPresents: state.numberOfPresents +1};
+    default:
+      return state;
+  }
+
 }
